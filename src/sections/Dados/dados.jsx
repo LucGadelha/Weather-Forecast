d682555c5b006e4aa83dd7e1f5541239a39dd375
Lucas Gadelha <lucasgadelha582@gmail.com>
2024-03-22 15:11:54 -0400

    ajustes-finais

--- src/sections/Dados/dados.jsx
+++ src/sections/Dados/dados.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -9,78 +9,78 @@ import Cloud2 from "../../assets/cloud2.svg";
 import IconLocalz from "../../assets/IconLocalz.svg";
 
 const Dados = () => {
-  async function Consultar() {
-    const cep = document.querySelector("#cep").value;
-    const ViaCepUrl = `https://viacep.com.br/ws/${cep}/json/`;
-    await fetch(ViaCepUrl)
-      .then((response) => {
-        response.json().then(mostrarEndereco);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [cep, setCep] = useState("");
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+  const [cepResponse, setCepResponse] = useState({});
+  const [localResponse, setLocalResponse] = useState();
+  const [validated, setValidated] = useState(false);
 
-    const lat = document.querySelector("#lat").value;
-    const long = document.querySelector("#long").value;
-    const Weather = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m`;
-    await fetch(Weather)
-      .then((response) => {
-        response.json().then(mostrarPrev);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  function mostrarEndereco(data) {
-    let Logra = document.querySelector("#Logra");
-    let Bairro = document.querySelector("#Bairro");
-    let UF = document.querySelector("#UF");
-
-    if (data.erro) {
-      alert("CEP Incorreto");
+  const Validar = async (event) => {
+    const form = event.target;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      event.preventDefault();
     } else {
-      Logra.innerHTML = `<p>${data.logradouro}</p>`;
-      Bairro.innerHTML = `<p>${data.bairro}</p>`;
-      UF.innerHTML = `<p>${data.uf}</p>`;
-    }
-  }
+      event.preventDefault();
 
-  function mostrarPrev(data) {
-    let prev = document.querySelector("#RespPrev");
-    if (data.erro) {
-      alert("Informações de Latitude e Longitude incorretas");
-    } else {
-      prev.innerHTML = `Previsão de tempo de acordo com a região: ${data.hourly.temperature_2m[0]}°C`;
+      await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then((response) => response.json())
+        .then(async (data) => {
+          setCepResponse(data);
+          await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m`)
+            .then((dadosMeteo) => dadosMeteo.json())
+            .then((dataMeteo) => setLocalResponse(dataMeteo));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-  }
+    setValidated(true);
+  };
 
   return (
     <Container fluid>
       <Container>
-        <Form className="col-6 mx-auto bg-light rounded-4 mt-5 p-4 pb-2 bg-white" id="formulario" method="post">
+        <Form
+          noValidate
+          validated={validated}
+          onSubmit={Validar}
+          className="col-6 mx-auto bg-light rounded-4 mt-5 p-4 pb-2 bg-white"
+        >
           <Row className="m-0 p-5">
             <Form.Group className="mt-3">
               <Form.Label className="inter-400 fs-5 text-body">Primeiro Nome:</Form.Label>
               <Form.Control
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 type="text"
                 id="nome"
                 placeholder="Insira seu primeiro nome:"
                 className="bg-light border-0 opacity-75 inter-400"
+                required
               />
             </Form.Group>
             <Form.Group className="mt-3">
               <Form.Label className="inter-400">E-mail:</Form.Label>
               <Form.Control
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 type="email"
                 id="email"
                 placeholder="Insira seu e-mail"
                 className="bg-light border-0 opacity-75 inter-400"
+                required
               />
             </Form.Group>
             <Form.Group className="mt-3">
               <Form.Label className="inter-400">Digite o CEP:</Form.Label>
               <Form.Control
+                pattern="[0-9]{8}"
+                value={cep}
+                onChange={(e) => setCep(e.target.value)}
                 type="text"
                 id="cep"
                 placeholder="Insira o CEP"
@@ -93,6 +93,8 @@ const Dados = () => {
               <Row>
                 <Col>
                   <Form.Control
+                    value={lat}
+                    onChange={(e) => setLat(e.target.value)}
                     type="text"
                     id="lat"
                     placeholder="Latitude"
@@ -102,6 +104,8 @@ const Dados = () => {
                 </Col>
                 <Col>
                   <Form.Control
+                    value={long}
+                    onChange={(e) => setLong(e.target.value)}
                     type="text"
                     id="long"
                     placeholder="Longitude"
@@ -111,7 +115,7 @@ const Dados = () => {
                 </Col>
               </Row>
             </Form.Group>
-            <Button className=" shadow p-0" id="botao" type="submit" onClick={Consultar}>
+            <Button className=" shadow p-0" id="botao" type="submit">
               <p className="m-0 fs-4 p-0 inter-600">Acessar</p>
             </Button>
           </Row>
@@ -138,11 +142,15 @@ const Dados = () => {
               <p className="fs-5 inter-800 mb-0">Localidade/UF</p>
             </Col>
           </Row>
-          <Row className="col-9 mx-auto p-2 bg-white text-center rounded-bottom-5">
-            <Col id="Logra" className="fs-5 inter-800 mb-0"></Col>
-            <Col id="Bairro" className="fs-5 inter-800 mb-0"></Col>
-            <Col id="UF" className="fs-5 inter-800 mb-0"></Col>
-          </Row>
+          {cepResponse ? (
+            <Row className="col-9 mx-auto p-3 bg-white text-center rounded-bottom-5">
+              <Col className="inter-800 mb-0">{cepResponse.logradouro}</Col>
+              <Col className="inter-800 mb-0">{cepResponse.bairro}</Col>
+              <Col className="inter-800 mb-0">{cepResponse.uf}</Col>
+            </Row>
+          ) : (
+            console.log("Falta o cep")
+          )}
         </Container>
         <Container className="mt-5">
           <Row className="pt-5 pb-3 mx-auto">
@@ -156,9 +164,15 @@ const Dados = () => {
         </Container>
         <Container className="mb-5 p-0">
           <Row className="col-9 mx-auto p-2 bg-white text-center rounded-5">
-            <Col className="p-2 d-flex justify-content-center align-items-center">
-              <p id="RespPrev" className=" mb-0 fs-5 inter-800"></p>
-            </Col>
+            {localResponse ? (
+              <Col className="d-flex align-items-center justify-content-around p-2 ">
+                <p className="inter-800 mb-0">
+                  Previsão de tempo de acordo com a região: {localResponse.hourly.temperature_2m[0]}Cº
+                </p>
+              </Col>
+            ) : (
+              ""
+            )}
           </Row>
         </Container>
       </Container>
